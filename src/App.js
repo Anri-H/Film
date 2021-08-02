c3d149fe1467aa9cd5c1105be98f0dc911dde8e2
Anri <anrihunanyan@gmail.com>
2021-08-03 00:19:21 +0400

    feat(app): changed background color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.less';
+import "./App.less";
 import Login from "./components/autorization/Login";
+// import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      {/* <NavBar /> */}
       <Login />
     </div>
   );
