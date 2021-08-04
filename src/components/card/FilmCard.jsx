89253cae19c8b2a958b8cbb92c65ccf0f9631b0b
Anri <anrihunanyan@gmail.com>
2021-08-04 17:40:38 +0400

    feat(FilmCard.jsx): added favorite add icon

--- src/components/card/FilmCard.jsx
+++ src/components/card/FilmCard.jsx
@@ -1,24 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { PlusCircleOutlined } from "@ant-design/icons";
 import { Card, Col, Row } from "antd";
-import {  getImgUrl } from "../../service/filmsData";
+import { getImgUrl } from "../../service/filmsData";
 
 const { Meta } = Card;
 
-export default function FilmCard({films}) {
-  
+export default function FilmCard({ films }) {
   return (
-    <Row justify="space-around" align="middle">
-      {films.map(({ poster_path, title, release_date }) => {
+    <Row
+      justify="space-around"
+      style={{ width: "80%", margin: "auto" }}
+      align="middle"
+    >
+      {films.map(({ poster_path, title, release_date, id }) => {
         return (
-          <Col flex="240px" >
-            <Card
-              hoverable
-              tabList
-              style={{ width: 240, height: 450,  margin:20 }}
-              cover={<img alt={title} src={getImgUrl(poster_path)} />}
-            >
-              <Meta title={title} description={release_date} />
-            </Card>
-          </Col>
+          <Link to={`/film/${id}`}>
+            <Col flex="240px">
+              <Card
+                hoverable
+                tabList
+                style={{ width: 240, height: 450, margin: 20 }}
+                cover={<img alt={title} src={getImgUrl(poster_path)} />}
+              >
+                <Meta
+                  title={title}
+                  description={release_date}
+                  avatar={
+                    <PlusCircleOutlined
+                      style={{ fontSize: 40, color: "green" }}
+                    />
+                  }
+                />
+              </Card>
+            </Col>
+          </Link>
         );
       })}
     </Row>
