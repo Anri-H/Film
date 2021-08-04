9e7434a1657a9159a46557a27f2e90f255e52115
Anri <anrihunanyan@gmail.com>
2021-08-04 17:39:35 +0400

    feat(Movies.jsx): added router link

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { useEffect, useState } from "react";
 import NavBar from "../navbar/NavBar";
 import FilmCard from "../card/FilmCard";
@@ -6,6 +9,7 @@ import {
   getMovieByQuery,
   getImgUrl,
 } from "../../service/filmsData";
+import Film from "../film/Film";
 
 export default function Movies() {
   const [films, setFilms] = useState([]);
@@ -13,25 +17,44 @@ export default function Movies() {
 
   useEffect(() => {
     search
-      ? getMovieByQuery(search).then(({ results }) => {
-          // const filmsData = results.map((res) => {
-          //   Object.assign({ img: getImgUrl(res.poster_path), }, res);
-          // });
-          
-          setFilms(results);
-        })
-      : getAllMovies().then(({ results }) => {
-          // const filmsData = results.map((res) => {
-          //   Object.assign({ img: getImgUrl(res.poster_path), }, res);
-          // });
-          setFilms(results);
-        });
+      ? getMovieByQuery(search)
+          .then(({ results }) => {
+            // const filmsData = results.map((res) => {
+            //   Object.assign({ img: getImgUrl(res.poster_path), }, res);
+            // });
+
+            setFilms(results);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      : getAllMovies()
+          .then(({ results }) => {
+            // const filmsData = results.map((res) => {
+            //   Object.assign({ img: getImgUrl(res.poster_path), }, res);
+            // });
+            console.log(results);
+            setFilms(results);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
   }, [search]);
 
   return (
     <>
       <NavBar onChange={setSearch} />
-      <FilmCard films={films} />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <FilmCard films={films} />
+          </Route>
+
+          <Route path="/film/:id" exact>
+            <Film />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
