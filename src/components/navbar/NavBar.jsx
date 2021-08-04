06e7350afdb840b316ec2b51db2aad7e17075685
Anri <anrihunanyan@gmail.com>
2021-08-04 17:38:57 +0400

    fix(NavBar.jsx): corrected navbar items

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -10,15 +10,15 @@ export default function NavBar({onChange}) {
   onChange(target.value);
 };
   return (
-    <Menu style={{ padding: "20px", backgroundColor: "#333" }}>
-      <Row  justify="space-between" align="middle">
-        <Col flex="300px" style={{height:"100px"}} >
+    <Menu style={{ padding: "10px", backgroundColor: "#333" }}>
+      <Row  justify="space-around" align="middle">
+        <Col flex="250px" style={{ margin:"30px 0"}} >
           <Logo />
         </Col>
-        <Col flex="500px" >
+        <Col flex="300px" >
           <Input placeholder="Search" onChange={onSearch} />
         </Col>
-        <Col flex="150px" >
+        <Col flex="150px">
           <LikeOutlined className="icon" />    
           <LogoutOutlined className="icon" />
         </Col>
