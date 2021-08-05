af7382fd21d79de79ed36e3ead1ce8f6881720fb
Anri <anrihunanyan@gmail.com>
2021-08-05 18:59:05 +0400

    feat(Film.jsx): added in film component film data

--- src/components/film/Film.jsx
+++ src/components/film/Film.jsx
@@ -1,5 +1,27 @@
-export default function Film({film}){
-    return (
-        <h1>hi</h1>
-    )
+import React from "react";
+import {  useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMovieById, getImgUrl } from "../../service/filmsData";
+import { Descriptions } from "antd";
+import DescriptionsItem from "antd/lib/descriptions/Item";
+
+export default function Film() {
+  let { id } = useParams();
+  const [film, setFilm] = useState();
+
+  getMovieById(id)
+    .then((data) => {
+      setFilm(data)
+    })
+    .catch((err) => console.log(err));
+
+  return (
+      <Descriptions
+    title="film"
+    bordered
+    column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+  >
+    <DescriptionsItem label="Product">{film.title}</DescriptionsItem>
+    </Descriptions >
+  );
 }
